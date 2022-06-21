f7ecb089db1858f7a0e960346feccbe81b918e90
tulparpos <85997699+tulparpos@users.noreply.github.com>
2022-06-22 00:01:50 +0300

    Validatior sınıflar hazırlandı.

---
+++ src/MK.Accountancy.Application.Contracts/BankDepartments/CreateBankDepertmentDtoValidator.cs
@@ -0,0 +1,34 @@
+﻿using FluentValidation;
+using Microsoft.Extensions.Localization;
+using MK.Accountancy.Consts;
+using MK.Accountancy.Localization;
+using System;
+
+namespace MK.Accountancy.BankDepartments
+{
+    public class CreateBankDepertmentDtoValidator : AbstractValidator<CreateBankDepartmentDto>
+    {
+        public CreateBankDepertmentDtoValidator(IStringLocalizer<AccountancyResource> localizer)
+        {
+            RuleFor(x => x.Code)
+                .NotEmpty()
+                .WithMessage(localizer[AccountancyDomainErrorCodes.Required, localizer["Code"]])
+                .MaximumLength(EntityConst.MaxCodeLength)
+                .WithMessage(localizer[AccountancyDomainErrorCodes.MaxLength, localizer["Code"], EntityConst.MaxCodeLength]);
+            //
+            RuleFor(x => x.Name)
+                .NotEmpty()
+                .WithMessage(localizer[AccountancyDomainErrorCodes.Required, localizer["Name"]])
+                .MaximumLength(EntityConst.MaxNameLength)
+                .WithMessage(localizer[AccountancyDomainErrorCodes.MaxLength, localizer["Name"], EntityConst.MaxNameLength]);
+            //
+            RuleFor(x => x.BankId)
+                .Must(x => x.HasValue && x.Value != Guid.Empty)
+                .WithMessage(localizer[AccountancyDomainErrorCodes.Required, localizer["Bank"]]);
+            //
+            RuleFor(x => x.Description)
+                .MaximumLength(EntityConst.MaxDescriptionLength)
+                .WithMessage(localizer[AccountancyDomainErrorCodes.MaxLength, localizer["Description"], EntityConst.MaxDescriptionLength]);
+        }
+    }
+}
