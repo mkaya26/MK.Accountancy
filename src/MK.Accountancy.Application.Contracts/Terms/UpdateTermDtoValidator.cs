70c87c70eed372c1e09891763f57de27c9a1a1cd
mkaya26 <kaya.esk@gmail.com>
2022-07-25 17:01:08 +0300

    Dönem validator sınıfları oluşturuldu.

---
+++ src/MK.Accountancy.Application.Contracts/Terms/UpdateTermDtoValidator.cs
@@ -0,0 +1,29 @@
+﻿using FluentValidation;
+using Microsoft.Extensions.Localization;
+using MK.Accountancy.Consts;
+using MK.Accountancy.Localization;
+
+namespace MK.Accountancy.Terms
+{
+    public class UpdateTermDtoValidator : AbstractValidator<UpdateTermDto>
+    {
+        public UpdateTermDtoValidator(IStringLocalizer<AccountancyResource> localizer)
+        {
+            RuleFor(x => x.Code)
+                .NotEmpty()
+                .WithMessage(localizer[AccountancyDomainErrorCodes.Required, localizer["Code"]])
+                .MaximumLength(EntityConst.MaxCodeLength)
+                .WithMessage(localizer[AccountancyDomainErrorCodes.MaxLength, localizer["Code"], EntityConst.MaxCodeLength]);
+            //
+            RuleFor(x => x.Name)
+                .NotEmpty()
+                .WithMessage(localizer[AccountancyDomainErrorCodes.Required, localizer["Name"]])
+                .MaximumLength(EntityConst.MaxNameLength)
+                .WithMessage(localizer[AccountancyDomainErrorCodes.MaxLength, localizer["Name"], EntityConst.MaxNameLength]);
+            //
+            RuleFor(x => x.Description)
+                .MaximumLength(EntityConst.MaxDescriptionLength)
+                .WithMessage(localizer[AccountancyDomainErrorCodes.MaxLength, localizer["Description"], EntityConst.MaxDescriptionLength]);
+        }
+    }
+}
