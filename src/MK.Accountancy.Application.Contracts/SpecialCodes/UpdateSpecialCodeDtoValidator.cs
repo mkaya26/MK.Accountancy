cfb4866970091c280e3bbcad5626dde63474acb0
mkaya26 <kaya.esk@gmail.com>
2022-07-27 14:06:45 +0300

    Özel kod validation sınıfları oluşturuldu.

---
+++ src/MK.Accountancy.Application.Contracts/SpecialCodes/UpdateSpecialCodeDtoValidator.cs
@@ -0,0 +1,41 @@
+﻿using FluentValidation;
+using Microsoft.Extensions.Localization;
+using MK.Accountancy.Consts;
+using MK.Accountancy.Localization;
+
+namespace MK.Accountancy.SpecialCodes
+{
+    public class UpdateSpecialCodeDtoValidator : AbstractValidator<UpdateSpecialCodeDto>
+    {
+        public UpdateSpecialCodeDtoValidator(IStringLocalizer<AccountancyResource> localizer)
+        {
+            RuleFor(x => x.Code)
+                .NotEmpty()
+                .WithMessage(localizer[AccountancyDomainErrorCodes.Required, localizer["Code"]])
+                .MaximumLength(EntityConst.MaxCodeLength)
+                .WithMessage(localizer[AccountancyDomainErrorCodes.MaxLength, localizer["Code"], EntityConst.MaxCodeLength]);
+            //
+            RuleFor(x => x.Name)
+                .NotEmpty()
+                .WithMessage(localizer[AccountancyDomainErrorCodes.Required, localizer["Name"]])
+                .MaximumLength(EntityConst.MaxNameLength)
+                .WithMessage(localizer[AccountancyDomainErrorCodes.MaxLength, localizer["Name"], EntityConst.MaxNameLength]);
+            //
+            RuleFor(x => x.SpecialCodeType)
+                .IsInEnum()
+                .WithMessage(localizer[AccountancyDomainErrorCodes.Required, localizer["SpecialCodeType"]])
+                .NotEmpty()
+                .WithMessage(localizer[AccountancyDomainErrorCodes.Required, localizer["SpecialCodeType"]]);
+            //
+            RuleFor(x => x.CardType)
+                .IsInEnum()
+                .WithMessage(localizer[AccountancyDomainErrorCodes.Required, localizer["CardType"]])
+                .NotEmpty()
+                .WithMessage(localizer[AccountancyDomainErrorCodes.Required, localizer["CardType"]]);
+            //
+            RuleFor(x => x.Description)
+                .MaximumLength(EntityConst.MaxDescriptionLength)
+                .WithMessage(localizer[AccountancyDomainErrorCodes.MaxLength, localizer["Description"], EntityConst.MaxDescriptionLength]);
+        }
+    }
+}
