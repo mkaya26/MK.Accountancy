2beb5f6414182ed42013b99358bab0ba8c054def
mkaya26 <kaya.esk@gmail.com>
2022-08-01 17:37:17 +0300

    Textedit componenti için çalışma yapıldı.

---
+++ src/MK.Blazor.Core/Components/DevComp/DataEditors/Base/DevBaseEditor.cs
@@ -0,0 +1,21 @@
+﻿using DevExpress.Blazor;
+using Microsoft.AspNetCore.Components;
+
+namespace MK.Blazor.Core.Components.DevComp.DataEditors.Base
+{
+    public class DevBaseEditor : ComponentBase
+    {
+        public DataEditorClearButtonDisplayMode ClearButtonDisplayMode { get; set; }
+        public int ColumnIndex { get; set; }
+        public int ColumnSpan { get; set; }
+        public string EditorCssClass { get; set; }
+        public bool Enabled { get; set; }
+        public string InputCssClass { get; set; }
+        public string LayoutItemCssClass { get; set; }
+        public string NullText { get; set; }
+        public bool ReadOnly { get; set; }
+        public int RowIndex { get; set; }
+        public int RowSpan { get; set; }
+        public bool Visible { get; set; }
+    }
+}
