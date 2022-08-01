8ae6c617fd904f2aa19cfa7631e4cdd8f98821c4
mkaya26 <kaya.esk@gmail.com>
2022-08-01 19:10:44 +0300

    Texedit componenti için çalışma yapıldı.

--- src/MK.Blazor.Core/Components/DevComp/DataEditors/Base/DevBaseEditor.cs
+++ src/MK.Blazor.Core/Components/DevComp/DataEditors/Base/DevBaseEditor.cs
@@ -1,21 +1,21 @@
 ﻿using DevExpress.Blazor;
-using Microsoft.AspNetCore.Components;
+using DevExpress.Blazor.Base;
 
 namespace MK.Blazor.Core.Components.DevComp.DataEditors.Base
 {
-    public class DevBaseEditor : ComponentBase
+    public class DevBaseEditor : DxComponentBase
     {
-        public DataEditorClearButtonDisplayMode ClearButtonDisplayMode { get; set; }
+        public DataEditorClearButtonDisplayMode ClearButtonDisplayMode { get; set; } = DataEditorClearButtonDisplayMode.Auto;
         public int ColumnIndex { get; set; }
         public int ColumnSpan { get; set; }
         public string EditorCssClass { get; set; }
-        public bool Enabled { get; set; }
+        public bool Enabled { get; set; } = true;
         public string InputCssClass { get; set; }
         public string LayoutItemCssClass { get; set; }
         public string NullText { get; set; }
         public bool ReadOnly { get; set; }
         public int RowIndex { get; set; }
         public int RowSpan { get; set; }
-        public bool Visible { get; set; }
+        public bool Visible { get; set; } = true;
     }
 }
