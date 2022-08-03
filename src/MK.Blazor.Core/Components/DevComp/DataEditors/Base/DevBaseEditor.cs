a203e3086d3281f7a3379aa0161aca82808faa68
mkaya26 <kaya.esk@gmail.com>
2022-08-03 12:02:56 +0300

    text edit componenti tamamlandı

--- src/MK.Blazor.Core/Components/DevComp/DataEditors/Base/DevBaseEditor.cs
+++ src/MK.Blazor.Core/Components/DevComp/DataEditors/Base/DevBaseEditor.cs
@@ -1,21 +1,28 @@
 ﻿using DevExpress.Blazor;
 using DevExpress.Blazor.Base;
+using Microsoft.AspNetCore.Components;
 
 namespace MK.Blazor.Core.Components.DevComp.DataEditors.Base
 {
     public class DevBaseEditor : DxComponentBase
     {
-        public DataEditorClearButtonDisplayMode ClearButtonDisplayMode { get; set; } = DataEditorClearButtonDisplayMode.Auto;
-        public int ColumnIndex { get; set; }
-        public int ColumnSpan { get; set; }
-        public string EditorCssClass { get; set; }
-        public bool Enabled { get; set; } = true;
-        public string InputCssClass { get; set; }
-        public string LayoutItemCssClass { get; set; }
-        public string NullText { get; set; }
-        public bool ReadOnly { get; set; }
-        public int RowIndex { get; set; }
-        public int RowSpan { get; set; }
-        public bool Visible { get; set; } = true;
+        [Parameter] public string Caption { get; set; }
+        [Parameter] public string CaptionCssClass { get; set; } = "caption";
+        [Parameter] public bool CaptionVisible { get; set; } = true;
+        [Parameter] public DataEditorClearButtonDisplayMode ClearButtonDisplayMode { get; set; } = DataEditorClearButtonDisplayMode.Auto;
+        [Parameter] public int ColumnIndex { get; set; }
+        [Parameter] public int ColumnSpan { get; set; }
+        [Parameter] public string EditorCssClass { get; set; }
+        [Parameter] public bool Enabled { get; set; } = true;
+        [Parameter] public string InputCssClass { get; set; }
+        [Parameter] public bool IsFocus { get; set; }
+        [Parameter] public string LayoutItemCssClass { get; set; }
+        [Parameter] public string NullText { get; set; }
+        [Parameter] public bool ReadOnly { get; set; }
+        [Parameter] public int RowIndex { get; set; }
+        [Parameter] public int RowSpan { get; set; }
+        [Parameter] public string SeperateCaption { get; set; } = ":";
+        [Parameter] public bool SeperateCaptionVisible { get; set; } = true;
+        [Parameter] public bool Visible { get; set; } = true;
     }
 }
