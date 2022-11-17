d97a6aebfce2cafb38c03b3a8804fecf3216f7ca
mkaya26 <kaya.esk@gmail.com>
2022-11-17 10:17:58 +0300

    buton edit companenti tasarlandı.

---
+++ src/MK.Blazor.Core/Services/ICoreCommonService.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace MK.Blazor.Core.Services
+{
+    public interface ICoreCommonService
+    {
+        public Action HasChanged { get; set; }
+        public ComponentBase ActiveEditComponent { get; set; }
+    }
+}
