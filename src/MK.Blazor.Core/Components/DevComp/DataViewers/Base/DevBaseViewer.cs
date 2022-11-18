61684fa6d85054234cdd6d8eff8f5cb822b4d359
mkaya26 <kaya.esk@gmail.com>
2022-11-18 06:51:52 +0300

    Datagrid componeneti tasasrlandı.

---
+++ src/MK.Blazor.Core/Components/DevComp/DataViewers/Base/DevBaseViewer.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace MK.Blazor.Core.Components.DevComp.DataViewers.Base
+{
+    public abstract class DevBaseViewer : ComponentBase
+    {
+        [Parameter] public int ColumnIndex { get; set; } = 0;
+        [Parameter] public int ColumnSpan { get; set; } = 0;
+        [Parameter] public int RowIndex { get; set; } = 0;
+        [Parameter] public int RowSpan { get; set; } = 0;
+    }
+}
---
+++ src/MK.Blazor.Core/Services/ICoreDataGridService.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace MK.Blazor.Core.Services
+{
+    public interface ICoreDataGridService<TDataGridItem>
+    {
+        public ComponentBase DataGrid { get; set; }
+        public IList<TDataGridItem> DataSource { get; set; }
+        public IEnumerable<TDataGridItem> SelectedItems { get; set; }
+        public bool ShowFilterRow { get; set; }
+        public bool ShowGroupPanel { get; set; }
+        public TDataGridItem SelectedItem { get; set; }
+    }
+}
