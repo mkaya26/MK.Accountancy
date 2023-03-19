c4d6bacbce4d351587fb44e03e9884e4011a5c18
Murat Kaya <91426358+mkaya26@users.noreply.github.com>
2023-03-19 21:38:18 +0300

    MHSB-17

---
+++ src/MK.Accountancy.Blazor/Pages/Invoices/SalesInvoiceListPage.razor.cs
@@ -0,0 +1,48 @@
+﻿using MK.Accountancy.Blazor.Services;
+using MK.Accountancy.Invoices;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System;
+using System.Linq;
+
+namespace MK.Accountancy.Blazor.Pages.Invoices
+{
+    public partial class SalesInvoiceListPage
+    {
+        public AppService AppService { get; set; }
+        protected override async Task GetListDataSourceAsync()
+        {
+            Service.ListDataSource = (await GetListAsync(new InvoiceListParameterDto
+            {
+                InvoiceType = InvoiceType.Sell,
+                DepartmentId = AppService.CompanyParameter.DepartmentId,
+                TermId = AppService.CompanyParameter.TermId,
+                Active = Service.IsActiveCards
+            })).Items.ToList();
+            //
+            Service.IsLoaded = true;
+        }
+
+        protected override async Task BeforeInsertAsync()
+        {
+            Service.DataSource = new SelectInvoiceDto
+            {
+                InvoiceNumber = await GetCodeAsync(new InvoiceNumberParameterDto
+                {
+                    InvoiceType = InvoiceType.Sell,
+                    DepartmentId = AppService.CompanyParameter.DepartmentId,
+                    TermId = AppService.CompanyParameter.TermId,
+                    Active = Service.IsActiveCards
+                }),
+                InvoiceType = InvoiceType.Sell,
+                InvoiceDate = DateTime.Now.Date,
+                DepartmentId = AppService.CompanyParameter.DepartmentId,
+                TermId = AppService.CompanyParameter.TermId,
+                Active = Service.IsActiveCards,
+                InvoiceDetails = new List<SelectInvoiceDetailDto>()
+            };
+            //
+            Service.ShowEditPage();
+        }
+    }
+}
