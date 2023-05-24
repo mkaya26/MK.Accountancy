fd32224217be462605a094005fc5addd23283769
Murat Kaya <91426358+mkaya26@users.noreply.github.com>
2023-05-24 14:44:03 +0300

    MHSB-52

---
+++ src/MK.Accountancy.Blazor/Reports/Receipts/CollectionReceipts/CollectionReceipt.cs
@@ -0,0 +1,13 @@
+using System;
+using DevExpress.XtraReports.UI;
+
+namespace MK.Accountancy.Blazor.Reports.Receipts.CollectionReceipts
+{
+    public partial class CollectionReceipt
+    {
+        public CollectionReceipt()
+        {
+            InitializeComponent();
+        }
+    }
+}
