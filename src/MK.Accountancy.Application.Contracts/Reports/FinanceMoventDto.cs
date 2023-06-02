b9628572756d534d03a943915e320cb78b5b03fb
Murat Kaya <91426358+mkaya26@users.noreply.github.com>
2023-06-02 11:38:42 +0300

    MHSB-64

---
+++ src/MK.Accountancy.Application.Contracts/Reports/FinanceMoventDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Volo.Abp.Application.Dtos;
+
+namespace MK.Accountancy.Reports
+{
+    public class FinanceMoventDto : IEntityDto
+    {
+        public DateTime Date { get; set; }
+        public string ReceiptNo { get; set; }
+        public decimal Debt { get; set; }
+        public decimal Receivable { get; set; }
+        public string Description { get; set; }
+    }
+}
---
+++ src/MK.Accountancy.Application.Contracts/Reports/FinancialStatusDto.cs
@@ -0,0 +1,10 @@
+﻿using Volo.Abp.Application.Dtos;
+
+namespace MK.Accountancy.Reports
+{
+    public class FinancialStatusDto : IEntityDto
+    {
+        public decimal Price { get; set; }
+        public string Description { get; set; }
+    }
+}
